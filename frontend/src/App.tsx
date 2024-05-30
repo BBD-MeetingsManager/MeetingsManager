06946706e4d0e63c6524c17af147dd24a7a07776
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-05-30 15:52:40 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import './App.css'
+import Navbar from './components/Navbar'
+import Landing from './pages/Landing'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Landing />,
+    },
+    {
+      path: 'home',
+      element: <Home />
+    }
+  ])
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#97aebb'
+      },
+      secondary: {
+        main: '#ff9233'
+      }
+    }
+  })
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
