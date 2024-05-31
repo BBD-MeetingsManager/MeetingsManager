13c5db9ce14fa7c4648357a85e3015621c33f56a
Kat <Katlego.Kungoane@bbd.co.za>
2024-06-01 01:06:31 +0200

    added redirect to sign in button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,14 @@ import { CalendarMonth } from "@mui/icons-material"
 import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material"
 
 const Navbar = () => {
+    const buttonOnClick = () => {
+        const hostedUiURL = "https://meeting-manager.auth.eu-west-1.amazoncognito.com";
+        const clientID = '5hv4ev8ff59uqven58ifeddtom';
+        const scopes = encodeURI("email openid phone");
+        const redirectUri = encodeURIComponent('http://localhost:5173/redirect');
+        window.location.href = `${hostedUiURL}/login?client_id=${clientID}&response_type=code&scope=${scopes}&redirect_uri=${redirectUri}`;
+    };
+
     return (
         <Box>
             <AppBar className="absolute top-0">
@@ -18,7 +26,7 @@ const Navbar = () => {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Meeting Manager
                     </Typography>
-                    <Button color="inherit" href="/home">Sign in</Button>
+                    <Button color="inherit" onClick={buttonOnClick}>Sign in</Button>
                 </Toolbar>
             </AppBar>
         </Box>
