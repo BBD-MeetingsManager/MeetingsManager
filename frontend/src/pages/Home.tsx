ad59741c353d23e0bcf32e87ae54cd61ddbbd1c8
Kat <Katlego.Kungoane@bbd.co.za>
2024-06-01 07:04:46 +0200

    implemented api call for loading upcoming meetings

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,37 +1,61 @@
 import { List, ListItemText, Typography } from "@mui/material"
 import { CalendarComponent } from "../components/CalendarComponent"
-import { dummyMeetings } from "../dummyData"
-import dayjs from "dayjs"
+import { paths } from "../enums/paths"
+import { format } from 'date-fns';
+import {useEffect, useState} from "react";
 
 const Home = () => {
+    const [upcomingMeetings, setUpcomingMeetings] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('id_token')}`,
+            },
+        };
+
+        const url = `${paths.apiUrlLocal}/complex/getMeetings`;
+        fetch(url, options)
+            .then(result => result.json()
+                .then(meetings => {
+                    const tmpMeetings = [];
+                    for (const meeting of meetings) {
+                        const formattedDate = format(new Date(meeting.startTime), 'yyyy-MM-dd');
+                        tmpMeetings.push(
+                            <ListItemText
+                                key={`upcoming-meeting-${meeting.meetingID}`}
+                                primary={`${meeting.title}`}
+                                secondary={
+                                    <>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            {formattedDate.toString()}
+                                        </Typography>
+                                        {/*Todo, there is proper CSS to do this ellipsis*/}
+                                        {` — ${meeting.description || ''} ...`}
+                                    </>
+                                }
+                                className="bg-paynes-gray-700/50 rounded-md p-4 shadow-md"
+                            />
+                        )
+                    }
+
+                    setUpcomingMeetings(tmpMeetings);
+                }));
+    }, []);
+
     return (
         <main className="w-full h-screen pt-16 bg-anti-flash-white">
             <section className="w-full grid grid-cols-2 gap-4 p-8">
                 <section className="w-10/12 h-full p-4 bg-paynes-gray-700/30 rounded-xl shadow-xl">
                     <h6 className="text-2xl">Upcoming meetings</h6>
                     <List className="gap-2">
-
-                        { // n.sort((a, b) => (dayjs(a).isAfter(dayjs(b)) ? 1 : -1))
-                            dummyMeetings.sort((a, b) => dayjs(a.date).isAfter(dayjs(b.date)) ? 1 : -1).map((meeting) => (
-                                <ListItemText
-                                    primary={`${meeting.title}`}
-                                    secondary={
-                                        <>
-                                            <Typography
-                                                sx={{ display: 'inline' }}
-                                                component="span"
-                                                variant="body2"
-                                                color="text.primary"
-                                            >
-                                                {meeting.date}
-                                            </Typography>
-                                            {` — ${meeting.description} ...`}
-                                        </>
-                                    }
-                                    className="bg-paynes-gray-700/50 rounded-md p-4 shadow-md"
-                                />
-                            ))}
-
+                        {upcomingMeetings}
                     </List>
                 </section>
                 <section className="w-full">
