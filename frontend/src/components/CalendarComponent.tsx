1fd7523d2d17dd6957c81e32b17945f589d867c8
Kat <Katlego.Kungoane@bbd.co.za>
2024-06-06 17:38:25 +0200

    fixed submits

--- frontend/src/components/CalendarComponent.tsx
+++ frontend/src/components/CalendarComponent.tsx
@@ -238,7 +238,6 @@ export const CalendarComponent = (props: CalendarComponentProps) => {
 
       fetch(url.toString(), options).then((result) =>
         result.json().then((asJson) => {
-          console.log('Created meeting', asJson);
           handleClose();
           setGetUpcomingMeetingsCount((prevState) => prevState + 1);
         })
