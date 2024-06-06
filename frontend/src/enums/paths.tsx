18bc8b6a2f0b769ec46d6a7cbc7aaa16ceed180f
timoBBD <159038883+timoBBD@users.noreply.github.com>
2024-06-06 22:30:37 +0200

    Update paths.tsx

--- frontend/src/enums/paths.tsx
+++ frontend/src/enums/paths.tsx
@@ -3,7 +3,7 @@ export const paths = {
     home: '/home',
     redirect: '/redirect',
     signOut: '/sign-out',
-    apiUrlLocal: 'https://d3mzpdjvgexsaf.cloudfront.net'
+    apiUrlLocal: 'https://d2d6k1kxeoo8le.cloudfront.net'
     // apiUrlLocal: 'http://localhost:8080',
     // ec2InstanceLocal: 'http://localhost:8080',
     // ec2InstanceLocal: 'http://ec2-34-248-128-133.eu-west-1.compute.amazonaws.com:8080',
