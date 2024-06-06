78d6cf2322d8cc9ceda139d9bfa5f2659dec29ba
Work Account <workaccount@Timos-MacBook-Air.local>
2024-06-06 14:30:48 +0200

    proxy server for frontend

--- frontend/src/enums/paths.tsx
+++ frontend/src/enums/paths.tsx
@@ -3,5 +3,5 @@ export const paths = {
     home: '/home',
     redirect: '/redirect',
     signOut: '/sign-out',
-    apiUrlLocal: 'http://localhost:8080'
+    apiUrlLocal: 'http://ec2-34-248-128-133.eu-west-1.compute.amazonaws.com:8080'
 };
