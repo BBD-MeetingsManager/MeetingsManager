1003e00b4e09a5d4648aa21603c68c28213da463
Work Account <workaccount@Timos-MacBook-Air.local>
2024-06-06 16:12:00 +0200

    updating to https

--- frontendServerProxy/server.js
+++ frontendServerProxy/server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-const PORT = process.env.PORT || 5173;
+const PORT = 443;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
